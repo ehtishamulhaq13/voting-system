3f7b9a7a0863ef1709540f60fc30c82a7c4eeeab
ehtishamulhaq13 <ehtishamidrees49@gmail.com>
2026-09-05 16:43:47 +0500

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -5,6 +5,11 @@ composer install --no-dev --working-dir=/var/www/html
 
 mkdir -p /var/www/html/database
 touch /var/www/html/database/database.sqlite
+mkdir -p /var/www/html/storage/framework/cache/data
+mkdir -p /var/www/html/storage/framework/sessions
+mkdir -p /var/www/html/storage/framework/views
+mkdir -p /var/www/html/storage/logs
+chmod -R 775 /var/www/html/storage
 
 echo "Caching config..."
 php artisan config:cache
